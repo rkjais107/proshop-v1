d79afbbab69973929cff2a7c6100e0afcbe65844
manas5996 <manas5996@gmail.com>
2022-11-05 17:04:25 +0530

    after fetching products from react-useEffect-

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+// import products from '../products'
 import { useParams } from 'react-router-dom'
 
 const ProductScreen = ({ match }) => {
+  const [product, setProduct] = useState({})
   const params = useParams();
   const product_id = params.id;
-  const product = products.find((p) => p._id === product_id)
+  // const product = products.find((p) => p._id === product_id)
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${product_id}`)
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [])
+
+
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
