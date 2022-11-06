743593becda41d3c62084d92bb942f73a07edea9
manas5996 <manas5996@gmail.com>
2022-11-06 12:54:13 +0530

    After bringing redux state into homescreen - usedispatch and useselector

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,24 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
-// import products from '../products'
 import Product from '../components/Product'
+import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
-  
+  const dispatch = useDispatch()
+
+  const productList = useSelector(state => state.productList)
+  const { loading, error, products } = productList
+
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products')
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
 
   return (
     <>
         <h1>Latest Products</h1>
-        <Row>
+        {loading ? <h2>Loading...</h2> : error ? <h3>{error}</h3> : <Row>
             {
                 products.map((product) => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -26,7 +25,8 @@ const HomeScreen = () => {
                     </Col>
                 ))
             }
-        </Row>
+        </Row>}
+        
     </>
   )
 }
