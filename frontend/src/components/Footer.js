c72820ea45ef383f76e3fa748b812c513cd0599d
manas5996 <manas5996@gmail.com>
2022-11-07 21:47:12 +0530

    editing title page

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <Container>
       <Row>
         <Col className='text-center py-3'>
-          Copyright &copy; ProShop Inc
+          Copyright &copy; E-Plant Inc
         </Col>
       </Row>
       </Container>
