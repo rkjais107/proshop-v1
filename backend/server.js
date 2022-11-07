7ad41960cdaa89d2fe035bd338998df4f3208eee
manas5996 <manas5996@gmail.com>
2022-11-07 17:12:30 +0530

    After prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
@@ -13,12 +14,20 @@ connectDB()
 const app = express()
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('API is running.........')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running.........')
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)
