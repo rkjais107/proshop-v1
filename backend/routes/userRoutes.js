a91f68b281420ad3d5770451a84b0380eb6ec4df
manas5996 <manas5996@gmail.com>
2022-11-07 09:50:01 +0530

    After custom authentication middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
 const router = express.Router()
-import {authUser} from '../controllers/userController.js'
+import {authUser, getUserProfile} from '../controllers/userController.js'
+import {protect} from '../middleware/authMiddleware.js'
 
 // @desc    Auth user and get token
 // @route   POST /api/users/login
 // @access  Public
 router.post('/login', authUser)
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
