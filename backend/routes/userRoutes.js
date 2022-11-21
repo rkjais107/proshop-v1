3eb4df5ad709b21b160f35e502aed044235b9da9
manas5996 <manas5996@gmail.com>
2022-11-21 13:27:52 +0530

    After order controller and route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,21 +1,29 @@
 import express from "express";
-const router = express.Router()
-import {authUser, getUserProfile, registerUser, updateUserProfile} from '../controllers/userController.js'
-import {protect} from '../middleware/authMiddleware.js'
+const router = express.Router();
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
-router.post('/', registerUser)
+router.post("/", registerUser);
 
 // @desc    Auth user and get token
 // @route   POST /api/users/login
 // @access  Public
-router.post('/login', authUser)
+router.post("/login", authUser);
 
 // @desc    Get user profile and update user profile
 // @route   GET /api/users/profile and PUT /api/users/profile
 // @access  Private
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
-export default router
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,43 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import path from 'path'
-import colors from 'colors'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js'
-import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
-const __dirname = path.resolve()
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    })
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
 } else {
-    app.get('/', (req, res) => {
-        res.send('API is running.........')
-    })
+  app.get("/", (req, res) => {
+    res.send("API is running.........");
+  });
 }
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
