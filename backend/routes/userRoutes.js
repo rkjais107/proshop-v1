8609198e5ada2ac7afb45352e4e87c0865b98b13
manas5996 <manas5996@gmail.com>
2022-12-27 21:59:08 +0530

    After Admin User Delete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,11 +6,12 @@ import {
   registerUser,
   updateUserProfile,
   getUsers,
+  deleteUser,
 } from "../controllers/userController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 // @desc    Register a new user
-// @route   POST /api/users
+// @route   POST /api/users and GET /api/users
 // @access  Public
 router.route("/").post(registerUser).get(protect, admin, getUsers);
 
@@ -27,4 +28,8 @@ router
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
 
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+router.route("/:id").delete(protect, admin, deleteUser);
 export default router;
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { ORDER_LIST_MY_RESET } from "../constants/orderConstants";
 import {
+  USER_DELETE_FAIL,
+  USER_DELETE_REQUEST,
+  USER_DELETE_SUCCESS,
   USER_DETAILS_FAIL,
   USER_DETAILS_REQUEST,
   USER_DETAILS_RESET,
@@ -208,6 +211,39 @@ export const listUsers = () => async (dispatch, getState) => {
     // console.error(error.response.data.messages)
     dispatch({
       type: USER_LIST_FAIL,
+      payload:
+        error.response && error.response.data.messages
+          ? error.response.data.messages
+          : error.message,
+    });
+  }
+};
+
+export const deleteUser = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_DELETE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    await axios.delete(`/api/users/${id}`, config);
+
+    dispatch({
+      type: USER_DELETE_SUCCESS,
+    });
+  } catch (error) {
+    // console.error(error.response.data.messages)
+    dispatch({
+      type: USER_DELETE_FAIL,
       payload:
         error.response && error.response.data.messages
           ? error.response.data.messages
