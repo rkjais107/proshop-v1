4c9331dd9e3cd67eefa6d4240e143d5c51b0e16a
manas5996 <manas5996@gmail.com>
2023-01-03 11:20:44 +0530

    After creating and updating product endpoints

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,17 +4,23 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  updateProduct,
+  createProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 // @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
-router.route("/").get(getProducts);
+// @route   GET /api/products and POST /api/products
+// @access  Public and Private/Admin
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 
 // @desc    Fetch single product
-// @route   GET /api/products/:id and DELETE /api/products/:id
-// @access  Public
-router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
+// @route   GET /api/products/:id and DELETE /api/products/:id and PUT /api/products/:id
+// @access  Public and Private/Admin
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
